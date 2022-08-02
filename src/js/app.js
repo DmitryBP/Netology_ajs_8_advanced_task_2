bb38c52a57d7a67e37c4e13803e6a9f3081b50e2
TDx Studio <79811924115@yandex.ru>
2022-08-02 15:32:37 +0300

    ajs-8-task2 complited v.2

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
-/* 
+/*
 Map
 Легенда
-Вы решили упорядочить работу с ошибками в своём приложении, а именно ввести цетрализованно числовой код ошибки и её описание.
+Вы решили упорядочить работу с ошибками в своём приложении,
+а именно ввести цетрализованно числовой код ошибки и её описание.
 И при генерации любых ошибок в коде приложения брать их из централизованного хранилища.
 
 Описание
@@ -11,28 +12,27 @@ Map
 Реализуйте метод translate(code), позволяющий по коду получить текст ошибки,
 а в случае отсутствия подобного кода возвращать строку 'Unknown error'.
 
-Не забудьте написать unit-тесты, которые обеспечивают 100% покрытие функций и классов, которые вы тестируете. 
+Не забудьте написать unit-тесты, которые обеспечивают 100% покрытие функций и классов,
+которые вы тестируете.
 */
 
 export default class ErrorRepository {
-    constructor() {
-        this.errorList = new Map;
-        this.errorList.set(101, 'Некая ошибка 101')
-        this.errorList.set(102, 'Некая ошибка 102')
-    }
+  constructor() {
+    this.errorList = new Map();
+    this.errorList.set(101, 'Некая ошибка 101');
+    this.errorList.set(102, 'Некая ошибка 102');
+  }
 
-    translate(code) {
-        if(this.errorList.get(code) !== undefined){
-            return this.errorList.get(code)
-        } else {
-            throw new Error('Unknown error')
-        }
+  translate(code) {
+    if (this.errorList.get(code) !== undefined) {
+      return this.errorList.get(code);
     }
+    throw new Error('Unknown error');
+  }
 }
 
-let errorRepository = new ErrorRepository;
+// const errorRepository = new ErrorRepository();
 
-console.log(errorRepository.translate(101))
-console.log(errorRepository.translate(102))
+// console.log(errorRepository.translate(101));
+// console.log(errorRepository.translate(102));
 // console.log(errorRepository.translate(103)) // throw new Error('Unknown error')
-
