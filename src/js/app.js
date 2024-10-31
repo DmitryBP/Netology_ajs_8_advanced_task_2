6dd44bc4d5fe28d5b9de0040fba172a2a77ee6ee
Dmitry <79811924115@yandex.ru>
2024-10-31 21:07:21 +0300

    Task2 ok

--- src/js/app.js
+++ src/js/app.js
@@ -1,38 +1,12 @@
-/*
-Map
-Легенда
-Вы решили упорядочить работу с ошибками в своём приложении,
-а именно ввести цетрализованно числовой код ошибки и её описание.
-И при генерации любых ошибок в коде приложения брать их из централизованного хранилища.
+/* eslint-disable no-unused-vars */
 
-Описание
-Создайте свой класс ErrorRepository внутри которого храните Map,
-в котором ключ - это код ошибки (число), а значение - текстовое описание (человекочитаемое).
-
-Реализуйте метод translate(code), позволяющий по коду получить текст ошибки,
-а в случае отсутствия подобного кода возвращать строку 'Unknown error'.
-
-Не забудьте написать unit-тесты, которые обеспечивают 100% покрытие функций и классов,
-которые вы тестируете.
-*/
-
-export default class ErrorRepository {
-  constructor() {
-    this.errorList = new Map();
-    this.errorList.set(101, 'Некая ошибка 101');
-    this.errorList.set(102, 'Некая ошибка 102');
-  }
-
-  translate(code) {
-    if (this.errorList.get(code) !== undefined) {
-      return this.errorList.get(code);
-    }
-    throw new Error('Unknown error');
-  }
-}
-
-// const errorRepository = new ErrorRepository();
-
-// console.log(errorRepository.translate(101));
-// console.log(errorRepository.translate(102));
-// console.log(errorRepository.translate(103)) // throw new Error('Unknown error')
+import Bowman from "./classes/Bowman";
+import Character from "./classes/Character";
+import Deamon from "./classes/Daemon";
+import Magician from "./classes/Magician";
+import Phonecontrol from "./classes/Phonecontrol";
+import Swordsman from "./classes/Swordsman";
+import Team from "./classes/Team";
+import Undead from "./classes/Undead";
+import Validator from "./classes/Validator";
+import Zombie from "./classes/Zombie";
